2eac7e302808b0a17dd1164300c4f10196bba9ac
Moises P. Sena <moisespsena@gmail.com>
2019-04-09 18:05:22 -0300

    Update user for root cmd

--- xb/cmd/root.go
+++ xb/cmd/root.go
@@ -26,7 +26,7 @@ var cfgFile string
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
-	Use:   "xb",
+	Use:   os.Args[0],
 	Short: "Extreme Binary Assets Tool for Go!",
 }
 
